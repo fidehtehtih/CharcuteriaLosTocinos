d4f324b74c1ee2e9b90cea16dc24ff27455e4e94
fidehtehtih <alexplanellasgarcia@gmail.com>
2022-05-12 16:25:39 +0200

    ok

--- src/app/php/connectbd.php
+++ src/app/php/connectbd.php
@@ -1,12 +1,12 @@
 <?php
-var promise = $http({method: 'POST', url: 'ruta/al/script/bd.php', data: losDatos});
+// var promise = $http({method: 'POST', url: 'ruta/al/script/bd.php', data: losDatos});
 
-promise.success(function (data, status, headers, config, statusText) {
-     //Este callback se realiza cuando la llamada ha sido un éxito
-});
+// promise.success(function (data, status, headers, config, statusText) {
+//      //Este callback se realiza cuando la llamada ha sido un éxito
+// });
 
-promise.error(function (data, status, headers, config, statusText) {
-     //Este callback se realiza cuando la llamada ha tenido un error
-});
+// promise.error(function (data, status, headers, config, statusText) {
+//      //Este callback se realiza cuando la llamada ha tenido un error
+// });
 
 ?>
