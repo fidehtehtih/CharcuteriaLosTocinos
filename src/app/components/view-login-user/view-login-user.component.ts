fd1535e21aeacc7211f1f92f57fa4aa1cef83d82
fidehtehtih <alexplanellasgarcia@gmail.com>
2022-05-16 16:20:22 +0200

    ok

--- src/app/components/view-login-user/view-login-user.component.ts
+++ src/app/components/view-login-user/view-login-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/objetos/usuario';
-import { createBrotliDecompress } from 'zlib';
+
 @Component({
   selector: 'app-view-login-user',
   templateUrl: './view-login-user.component.html',
@@ -12,7 +12,7 @@ usuario:any;
 
   constructor() { 
     
-    this.createbd();
+    // this.createbd(); 
 
   
 
@@ -21,43 +21,43 @@ usuario:any;
   ngOnInit(): void {
   }
 
-createbd(){
-  var mysql = require('mysql');
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-  });
+// createbd(){
+//   var mysql = require('mysql');
+//   var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: ""
+//   });
   
-  con.connect(function(err: any) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE if not exist charcuteriaTocinoss", function (err: any, result: any) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-  });
-}
+//   con.connect(function(err: any) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE if not exist charcuteriaTocinoss", function (err: any, result: any) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+// }
 
-createtables(){
-  var mysql = require('mysql');
+// createtables(){
+//   var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "charcuteriaTocinoss"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "charcuteriaTocinoss"
+// });
 
-con.connect(function(err: any) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err: any, result: any) {
-    if (err) throw err;
-    console.log("Table created");
-  });
-});
-}
+// con.connect(function(err: any) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err: any, result: any) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
+// }
 
 }
